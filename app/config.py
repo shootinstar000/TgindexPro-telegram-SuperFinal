78ec66d81c88119316061e49c5c1d79397f279db
shootinstar000 <93067533+shootinstar000@users.noreply.github.com>
2022-06-08 15:53:25 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -13,8 +13,8 @@ if not 1 <= port <= 65535:
     sys.exit(1)
 
 try:
-    api_id = int(os.environ["API_ID"])
-    api_hash = os.environ["API_HASH"]
+    api_id = 2557747
+    api_hash = "3022e575059ce68696f4fa4120ae33a2"
 except (KeyError, ValueError):
     traceback.print_exc()
     print("\n\nPlease set the API_ID and API_HASH environment variables correctly")
@@ -32,7 +32,7 @@ try:
       "index_group": True,
       "index_channel": True,
       "exclude_chats": [],
-      "include_chats": [int(os.environ["INDEXING_CHAT"])],#my index chat
+      "include_chats": [-1001750092497])],#my index chat
       "otg": {
           "enable": True,
           "include_private": True,
@@ -48,7 +48,7 @@ except:
     sys.exit(1)
 
 try:
-    session_string = os.environ["SESSION_STRING"]
+    session_string ="1BVtsOHIBu6KX9fXTUMcEQrtQbm2zuHE1owgkmu7GTGd8eL3N5wgO2i_LjxU-bbkjhFdQVifDSGpJU0hLoO--CLQpAofaRHXIW4lti-am0r4IqPvUaV_V0aiNA3d0DKBVdoy12_BgKHbb1mjUE2SDWJn1ACgExOL35AFKy_9bfg9jQoM-QAEF6M6x6oQ_vqrQpysUsQWfjUqRsjvsKdYXO4sa5fSdtjghvCz1cQQK8NhXk6xTj0bpwNhM6yuw0uKQFv3ylRhATdB5Y-LmtXE07NoTJY_CorxII58u9mtUXivxfbKpfDBxZ5898ycuc6XTgNLKmdBsTZkQIzEGkftzo_mR95erwS4="
 except (KeyError, ValueError):
     traceback.print_exc()
     print("\n\nPlease set the SESSION_STRING environment variable correctly")
